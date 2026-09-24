fe66c9841f009609fca51039cbe2e7277d90778d
S Ganesh Babu <sganeshbabu990@gmail.com>
2026-09-25 01:22:28 +0530

    Test GitHub webhook

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    res.end(`
-        <h1>AWS DevOps Project</h1>
-        <p>Application running inside Docker</p>
-        <p>Environment: Development</p>
-    `);
-});
-
-server.listen(3000, () => {
-    console.log('Application running on port 3000');
-});
+<p>Environment: Development</p>
+<p>Webhook Test: GitHub to Jenkins</p>
